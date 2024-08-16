a725f1d833e4d4982b437569792066bdc6a0e73a
Mark Lundin <mlundin@snapchat.com>
2024-08-16 13:18:51 +0100

    lowers strictness, and only returns syntactic errors

--- test/fixtures/program.valid.js
+++ test/fixtures/program.valid.js
@@ -3,6 +3,7 @@ import { Script } from 'playcanvas';
 // The parser should ignore https imports and not throw an error
 import confetti from "https://esm.sh/canvas-confetti@1.6.0"
 import * as TWEEN from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/23.1.2/tween.esm.js'
+
 class Example extends Script {
     /**
      * @attribute
@@ -12,6 +13,7 @@ class Example extends Script {
 
     initialize() {
         confetti();
+        window.localStorage.setItem('test', 'test');
         new TWEEN.Tween({ x: 0 }).to({ x: 100 }, 1000).start();
     }
 }
